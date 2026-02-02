409093dc71ce6a35f90f8b4acd99c1ff66e47f3a
zhouxun <dev.zhouxun@outlook.com>
2026-02-02 13:50:55 +0800

    初始化

---
+++ packages/blogpress/.vitepress/theme/Layout.vue
@@ -0,0 +1,12 @@
+<script setup>
+import ThemeLayout from '@sugarat/theme'
+import BlogFooter from '@sugarat/theme/src/components/BlogFooter.vue'
+</script>
+
+<template>
+  <ThemeLayout>
+    <template #footer>
+      <BlogFooter />
+    </template>
+  </ThemeLayout>
+</template>
